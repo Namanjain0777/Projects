4a117feb6b170240673d87b7a471dcf4bfddfa4d
Namanjain0777 <Namanjainp01@gmail.com>
2025-02-01 18:46:50 +0530

    Initial commit

---
+++ Task Manager/script.js
@@ -0,0 +1,7 @@
+const addtask = document.getElementById("addTask");
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+const all = document.getElementById("all");
+const completed = document.getElementById("completed");
+const pending = document.getElementById("pending");
+
